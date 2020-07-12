acf313320514a9c6d1ddaa236d92fae0e718b2f2
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-12 02:37:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -17,8 +17,8 @@ def oxford_comma (array)
  result = []
  array["kiwi", "durian", "starfruit"].join(", ") + ", and " + array[-1] do |string|
  puts string
-binding.pry
  end
+ binding.pry
 end
 
 #array=[]
